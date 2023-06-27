e49883d7635409fa114c0f0bd6667fa878e77ecd
kojunsei <135289324+kojunsei@users.noreply.github.com>
2023-06-27 16:00:49 +0900

    Update task6 finished

--- task6.js
+++ task6.js
@@ -1,21 +1,21 @@
 (() => {
 	'use strict';
-    const action5 = ['あくなき探求','不屈の心体','理想への共感','知識を増やす','公明正大'];
-    kintone.events.on('app.record.create.show',(event) => {
-		console.log(event);
-       const location = event.record.Table.value;
-	   const zero = event.record.Table.value[0];
-	   console.log(location);
-       
-	   action5.forEach((val) => {
-		zero.value.Action5.value =val;
+	const action5 = ['あくなき探求', '不屈の心体', '理想への共感', '心を動かす', '知識を増やす', '公明正大'];
+	kintone.events.on('app.record.create.show', (event) => {
+	  console.log(event);
+	  const location = event.record.Table.value;
+	  const zero = event.record.Table.value[0];
+	  console.log(location);
+	  console.log(zero);
+  
+	  action5.forEach((val) => {
+		zero.value.Action5.value = val;
 		const clone = structuredClone(zero);
-        location.push(clone);
-	   })
-	   
-	   location.shift();
-    return event;
-    } );
-	
-
-})();
+		location.push(clone);
+	  });
+  
+	  location.shift();
+	  return event;
+	});
+  
+  })();

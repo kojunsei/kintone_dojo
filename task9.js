5660d8670e62debdc06237dc27c2c5ab622f0d13
kojunsei <135289324+kojunsei@users.noreply.github.com>
2023-07-18 17:16:49 +0900

    Update task9.js

--- task9.js
+++ task9.js
@@ -1,44 +1,5 @@
 (() => {
 'use strict';
-  const eventype = ['app.record.create.show',
-    'app.record.create.change.日付',
-    'app.record.create.change.サイボウズ製品',
-    'app.record.create.change.管理番号',
-    'app.record.edit.show',
-    'app.record.edit.change.日付',
-    'app.record.edit.change.サイボウズ製品',
-    'app.record.edit.change.管理番号'];
-
-  // 普通に書き込み
-  kintone.events.on(eventype, (event) => {
-  // console.log(event);
-    const data = event.record.日付.value;
-    const product = event.record.サイボウズ製品.value;
-    const num = event.record.管理番号.value;
-    const record_word = event.record.重複禁止項目.value;
-
-    let product_show = '';
-    if (product === 'kintone') {
-      product_show = '-KN';
-    } else if (product === 'Garoon') {
-      product_show = '-GR';
-    } else if (product === 'サイボウズOffice') {
-      product_show = '-OF';
-    } else if (product === 'Mailwise') {
-      product_show = '-MW';
-    }
-    // console.log(product_show);
-    // 日にち
-    const formatDate = dateFns.format(data, 'YYYYMMDD');
-    const num_show = '-' + num;
-
-    const sum = formatDate + product_show + num_show;
-    // console.log(sum);
-    event.record.重複禁止項目.value = sum;
-
-    return event;
-  });
-
   //  保存している時のイベント
   const eventype2 = ['app.record.create.submit', 'app.record.edit.submit'];
   kintone.events.on(eventype2, async(event) => {
@@ -54,16 +15,12 @@
     }
 
     const body = {
-      app: 16,
+      app: kintone.app.record.getId(),
       // id:kintone.app.record.getId(),
       query: query,
     };
 
-
     const content2 = await kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', body);
-    console.log(content2);
-    console.log(content2.records.length);
-    console.log(content2.records.length);
 
     //  自分のIDが編集する際に、重複していると勘違いされることがある
     //  今自分のIDであればokというのを書く
